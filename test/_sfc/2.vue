69dc62677d7b3bca4ba6e9ce6ffc7462bb3e70f1
lbb00 <mr@lbb00.com>
2024-03-06 22:27:11 +0800

    feat(test): add a bad case

---
+++ test/_sfc/2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="title fz-14"></div>
+  <div class="title fz-14">
+    <div class="subtitle fz-12"></div>
+  </div>
+</template>
+
+<style lang="scss">
+.title + .active {
+  .subtitle {
+    font-size: 16px;
+  }
+}
+</style>
